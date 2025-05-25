0ed7c3abfc39bcdb10ae2b891f26eac60294dd20
Matisio <mateuszmatisio@gmail.com>
2025-05-25 18:35:41 +0200

    synchronizacja z główną stroną

--- strona/relivedebloat/11/Win11Debloat.ps1
+++
@@ -1,3 +0,0 @@
-& ([scriptblock]::Create((irm "https://debloat.raphi.re/"))) -RunDefaults -Silent
-Invoke-WebRequest -Uri 'https://reliveteam.eu/relivedebloat11/tapetairestart.exe' -OutFile 'C:\Program Files\Relive DeBloat11\tapetairestart.exe'
-start "" "C:\Program Files\Relive DeBloat11\tapetairestart.exe"
